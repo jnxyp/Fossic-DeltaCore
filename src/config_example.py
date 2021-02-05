fdf78afd037d7e69f0d119a77fdb630f30ddba38
jn_xyp <jinan.xyp@gmail.com>
2021-02-05 22:08:29 +0800

    Update example config file

---
+++ src/config_example.py
@@ -0,0 +1,9 @@
+config = {
+    'receivers': {
+        'graiaMirai': {
+            'account': 88888888,
+            'authKey': 'blablablabla',
+            'host': 'http://localhost:8080'
+        }
+    }
+}
